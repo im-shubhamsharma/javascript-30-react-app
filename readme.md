fe02cad62dfff39a4ba49e68a71cd3f4f280d0fa
im-shubhamsharma <im_shubhamsharma@outlook.com>
2022-08-29 06:23:22 +0530

    added responsive navbar to project

--- readme.md
+++ readme.md
@@ -4,12 +4,22 @@
 
 Hi there!
 
-This repository is created to keep track of my progress in the awesome JavaScript 30 Day Challenge
-and commiting & logging what I have learnt on daily basis.
+This repository is created to practice React and JavaScript Concepts I have learned so far and to keep track of my progress in the awesome JavaScript 30 Day Challenge and commiting & logging what I have learnt on daily basis.
 
+> I have a created a React app for this repo which will contain all javascript 30 projects I am going to learn and implement through out this course.
+
+## Tech Used
+
+- React JS
+- React Router
+- Javascript
+- SASS
+- CSS
+- HTML
 
 ## Logs
 
-> Day 1 - Aug 28, 2022 </br>
-> Projects Completed - Drum Kit & JS+CSS Clock </br> 
-> Practice/Learnings - Key and transitionend events, Audio element in HTML, DOM selectors,  Date Object, ForEach Loop, Array.from(create a shallow copy of an array), CSS transform and transition
+### Day 1 - Aug 28, 2022 </br>
+
+- Projects Completed - Drum Kit & JS+CSS Clock </br>
+- Practice/Learnings - Key and transitionend events, Audio element in HTML, DOM selectors, Date Object, ForEach Loop, Array.from(create a shallow copy of an array), CSS transform and transition </br>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import { CgMenu, CgClose } from "react-icons/cg";
 
 function Header(props) {
+  const showMenu = useRef();
+  const hideMenu = useRef();
+  const navbar = useRef();
+
+  function toggleShowMenu() {
+    showMenu.current.children[0].classList.remove("show-menu");
+    hideMenu.current.children[0].classList.add("hide-menu");
+    navbar.current.classList.add("responsive-navbar");
+  }
+
+  function toggleHideMenu() {
+    showMenu.current.children[0].classList.add("show-menu");
+    hideMenu.current.children[0].classList.remove("hide-menu");
+    navbar.current.classList.remove("responsive-navbar");
+  }
+
   return (
-    <>
+    <div className="header">
       <h1 className="logo">JavaScript30</h1>
-      <div className="header">
+      <nav ref={navbar}>
         <Link className="link" to="/">
           Projects
         </Link>
@@ -15,8 +32,15 @@ function Header(props) {
         <Link className="link" to="/about">
           About
         </Link>
+      </nav>
+      <div ref={showMenu}>
+        <CgMenu onClick={toggleShowMenu} className="menu-btn show-menu" />
       </div>
-    </>
+
+      <div ref={hideMenu}>
+        <CgClose onClick={toggleHideMenu} className="menu-btn" />
+      </div>
+    </div>
   );
 }
 
